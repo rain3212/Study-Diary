fbd47194c1102dd7ef9100d4c95825da05b5d6a4
W <167002827+rain3212@users.noreply.github.com>
2026-08-16 22:37:14 +0800

    合并拉取请求 #10
    * code:12 LED_EXIT 代码

---
+++ 12_led_exit/components/my_led/my_led.cpp
@@ -0,0 +1,15 @@
+
+#include "my_led.h"
+#include "driver/gpio.h"
+
+void led_init(void)
+{
+    gpio_config_t gpio_init_structure;
+    gpio_init_structure.mode = GPIO_MODE_OUTPUT;
+    gpio_init_structure.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_init_structure.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    gpio_init_structure.intr_type = GPIO_INTR_DISABLE;
+    gpio_init_structure.pin_bit_mask = 1ULL << LED_PIN;
+    gpio_config(&gpio_init_structure); //写到相关的寄存器中
+    LED(0); //初始默认状态
+}
